41d9a30af0c779c57511ea81c232b56db9195eda
santy20080 <sarevalo46@ucatolica.edu.co>
2026-04-22 11:35:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import json
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+with open("productos.json", "r") as f:
+    productos = json.load(f)
 
 productos = [
     {"codigo": 1, "nombre": "Cuaderno", "valoru": 5000, "existencias": 100},
